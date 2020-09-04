d97aeb1354254571ac8f0be5f150a2f7c9710d95
Jason Jones <jason.jones@swiftpage.com>
2020-09-04 09:39:05 -0700

    Getting started

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularTestBed'`, () => {
+  it(`should have as title 'Angular Testbed'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularTestBed');
+    expect(app.title).toEqual('Angular Testbed');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularTestBed app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular Testbed app is running!');
   });
 });
