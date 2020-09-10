a6029853218d4a7b13d7071f7b5004dc66ad8ebe
Jason Jones <jason.jones@swiftpage.com>
2020-09-10 15:19:06 -0700

    convert contacts to contactdisplay

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../../core/models';
 import { ContactDisplay } from '../../core/models';
-import { Observable, Subject, pipe } from 'rxjs';
+import { Observable, Subject, pipe, ReplaySubject, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { ContactService} from '../contact.service';
@@ -14,24 +14,31 @@ import { ContactDetailComponent } from '../contact-detail/contact-detail.compone
 })
 export class ContactListComponent implements OnInit {
 
-   contacts$: Observable<Contact[]>;
+   contacts$ = new BehaviorSubject<ContactDisplay[]>(null);
 
 
 
   constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
-    
-    this.contacts$ = this.contactService.getContacts();
-
-
-
+    this.getContactForDisplay();
   }
 
-  /*
-  getContactForDisplay(): Observable<ContactDisplay[] {
-
+  getContactForDisplay(): void {
+    this.contactService.getContacts()
+    .subscribe((contacts) => { 
+      const dispContacts: ContactDisplay[] =  contacts.map(c => {
+        const result: ContactDisplay = {
+          id: c.id,
+          firstName: c.firstName,
+          lastName: c.lastName,
+          displayName: `${c.firstName} ${c.lastName}`,
+          birthDate: c.birthDate
+        };
+        return result;
+      });
+      this.contacts$.next(dispContacts);
+    });
   }
-  */
 
 }
