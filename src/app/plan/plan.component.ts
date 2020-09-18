51fd80e34741574b247a7dd9119feabed69941a6
Jason Jones <jason.jones@swiftpage.com>
2020-09-18 13:37:43 -0700

    Getting the object creation working.

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -8,6 +8,23 @@ import { FeatureService } from './feature/feature.service';
 
 import { Plan, Feature } from '../core/models';
 
+export enum FeatureSupportLevel {
+  Yes,
+  No,
+  Custom
+}
+
+export interface FeatureSupport {
+  featureId: string;
+  support: FeatureSupportLevel;
+}
+
+export interface PlanFeature {
+  planId: string;
+  planName: string;
+  featureSupport: FeatureSupport[];
+}
+
 
 @Component({
   selector: 'app-plan',
@@ -18,7 +35,7 @@ export class PlanComponent implements OnInit {
 
   plans$ = new BehaviorSubject<Plan[]>(null);
   features$ = new BehaviorSubject<Feature[]>(null);
-
+  planFeatures$ = new BehaviorSubject<PlanFeature[]>(null);
 
   constructor(private planService: PlanService, private featureService: FeatureService) { }
 
@@ -30,13 +47,42 @@ export class PlanComponent implements OnInit {
         map((combined) => {
           this.plans$.next(combined.plans);
           this.features$.next(combined.features);
+
+          const planFeatures: PlanFeature[] = combined.plans.map((plan) => {
+            const planFeature: PlanFeature = {
+              planId: plan.id,
+              planName: plan.name,
+              featureSupport: this.getFeatureSupport(plan, combined.features)
+            };
+            return planFeature;
+          });
+
+          this.planFeatures$.next(planFeatures);
         })
       ).subscribe((data) => {
         console.log(data);
       });
-
-  
-  
   }
 
+  private getFeatureSupport(plan: Plan, features: Feature[]): FeatureSupport[] {
+
+    const featureSupportList: FeatureSupport[] = features.map((feature) => {
+
+      let supportLevel: FeatureSupportLevel = FeatureSupportLevel.No;
+
+      if (plan.standardFeatures.includes(feature.id)) {
+        supportLevel = FeatureSupportLevel.Yes;
+      } else if (plan.customFeatures.includes(feature.id)) {
+        supportLevel = FeatureSupportLevel.Custom;
+      }
+
+      const featureSupport: FeatureSupport = {
+        featureId: feature.id,
+        support: supportLevel,
+      };
+      return featureSupport;
+    });
+
+    return featureSupportList;
+  }
 }
